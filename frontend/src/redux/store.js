fc77c59b746f8d8b719ba6c773fa173ab11cb7f3
Bhupesh kumawat <bhupeshkumawat100@gmail.com>
2022-07-20 17:59:27 +0530

    init

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,24 @@
+import {legacy_createStore as createStore, combineReducer, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducer({
+
+});
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+
+const initialState = {};
+
+const store = createStore(rootReducer, initialState,enhancer);
+
+export default store;
