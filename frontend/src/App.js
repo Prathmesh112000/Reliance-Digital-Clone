4cc217600b3b5b0150205cfc81e58b1d55ae4137
Alim <alimabbas44891@gmail.com>
2022-07-21 23:06:33 +0530

    checkoutpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Navbar from './components/NavFolder/Navbar';
 import {Product} from './Pages/Product';
 import IndividualProduct from "./components/IndividualProduct";
+import Checkout from './Pages/Checkout';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Product/>
+      {/* <Navbar /> */}
+      {/* <Product/> */}
       {/* <h1>Hello</h1> */}
-      <IndividualProduct/>
+      {/* <IndividualProduct/> */}
+      <Checkout/>
     </div>
   );
 }
