8c354b683d64a2885f80f59c4874c26a379e9351
Prathmesh112000 <87255237+Prathmesh112000@users.noreply.github.com>
2022-07-22 00:17:24 +0530

    authentication added

---
+++ backend/models/usermodel.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose")
+
+var userSchema = new mongoose.Schema({
+    name:{ type: String, required: true},
+    email:{ type: String, required: true, index: { unique: true }},
+    password: { type: String, required: true },
+    mobile:{ type: String, required: true, index: { unique: true }},
+    orders:[]
+});
+
+const userModel=mongoose.model("user",userSchema)
+
+module.exports=userModel
