eed6e1b8e708713662c09dbe798d1217108956e6
Prathmesh112000 <87255237+Prathmesh112000@users.noreply.github.com>
2022-07-22 21:49:05 +0530

    signin added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/NavFolder/Navbar';
 import {Product} from './Pages/Product';
 import IndividualProduct from "./components/IndividualProduct";
 import Checkout from './Pages/Checkout';
+import Signup from "./components/Authentication/Signup"
+import Sign from './components/Authentication/Sign';
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       {/* <Product/> */}
       {/* <h1>Hello</h1> */}
       {/* <IndividualProduct/> */}
-      <Checkout/>
+      {/* <Checkout/> */}
+    <Signup/>
+    {/* <Sign/> */}
     </div>
   );
 }
---
+++ frontend/src/components/Authentication/Signup.jsx
@@ -0,0 +1,79 @@
+import React , {useState} from "react"
+import "./Signup.css"
+
+function Signup(){
+    const [formdata,setformdata]=useState({})
+    const onNameChange=(e)=>{
+      
+        const inputname=e.target.name
+        if(inputname=="name"){
+            setformdata({
+                ...formdata,
+                name:e.target.value
+            })
+            
+        }
+        if(inputname=="email"){
+            setformdata({
+                ...formdata,
+                email:e.target.value
+            })
+        }
+
+        if(inputname=="password"){
+            setformdata({
+                ...formdata,
+                password:e.target.value
+            })
+        }
+
+        if(inputname=="mobile"){
+            setformdata({
+                ...formdata,
+                mobile:e.target.value
+            })
+        }
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+       
+    }
+
+
+
+    return(<div style={{"paddingTop":"170px","paddingBottom":"20px"}}>
+
+    <div style={{"paddingBottom":"20px","borderRadius":"5px","margin":"auto","width":"35%","backgroundColor":"#ffffff","border":"1px solid black","height":"auto","boxShadow": "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"}}>
+    
+    <div>
+        <h1>Register New Account</h1>
+    </div>
+    <hr/>
+
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="First Name*" name="name" className="inpelem" onChange={onNameChange}/> <br/>
+            <input type="text" placeholder="Last Name*" name="lastname" className="inpelem" onChange={onNameChange}/> <br/>
+            <input type="email" placeholder="Email Address*" name="email" className="inpelem" onChange={onNameChange}/> <br/>
+            <p style={{"fontSize":"14.4px","marginLeft":"40px","lineHeight":"3px"}}>Your email address will be used to send order invoice, order updates etc.</p>
+            <button style={{"width":"190px", "marginLeft":"40px"}}>Verify Email</button>
+            <input type="number" placeholder="Mobile No*" className="inpelem" onChange={onNameChange}/> <br/>
+            <p style={{"fontSize":"14.4px","marginLeft":"40px"}}>Your mobile number will be used to avail benefits such as Jio  Mart <br/> Cashback and  ROne Loyality Points and receive quick notifications.</p>
+            <input type="password" placeholder="Password*" name="password" className="inpelem" onChange={onNameChange}/> <br/>
+            <input type="password" placeholder="Confirm Password*" name="confirmpass" className="inpelem"/> <br/>
+            {/* <input type="submit">Proceed</input> */}
+            <button style={{"width":"480px","marginTop":"30px","height":"30px","marginLeft":"42px"}}>Proceed</button>
+
+        </form>
+    </div>
+
+    </div>
+    
+   
+    </div>)
+   
+
+}
+
+export default Signup
